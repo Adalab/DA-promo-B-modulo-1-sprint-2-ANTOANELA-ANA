28a5009147f6d826eba0e6936c2996e576c1a162
Ana Delia Torres <anadeliats@gmail.com>
2022-07-26 19:34:37 +0200

    Ejercicio 3 de consultas multiples 1 resuelto

--- sql/pair-programming-mod-1-consultas-multiples-1.sql
+++ sql/pair-programming-mod-1-consultas-multiples-1.sql
@@ -20,7 +20,17 @@ WHERE country = "UK"
 GROUP BY empresa, Año;
 	
 
+# ejercicio 3: dinero perdido por pedidos en empresas de UK
 
+SELECT customers.company_name AS empresa, YEAR(orders.order_date) AS Año, SUM(order_details.quantity) AS cantidad_productos, 
+SUM(order_details.unit_price*order_details.quantity) - SUM(order_details.unit_price*order_details.quantity*order_details.discount) AS dinero_perdido
+FROM orders
+	INNER JOIN customers
+		ON orders.customer_id = customers.customer_id
+	INNER JOIN order_details
+		ON order_details.order_id = orders.order_id
+WHERE country = "UK"
+GROUP BY empresa, Año;
 
 
 
