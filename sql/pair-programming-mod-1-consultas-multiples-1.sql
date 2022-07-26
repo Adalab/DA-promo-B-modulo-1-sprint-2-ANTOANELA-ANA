553a558133b393eafca61402e42583a3eda6f52b
Ana Delia Torres <anadeliats@gmail.com>
2022-07-26 19:40:38 +0200

    Modificacion ejercicio 1 de consultas multiples 1

--- sql/pair-programming-mod-1-consultas-multiples-1.sql
+++ sql/pair-programming-mod-1-consultas-multiples-1.sql
@@ -4,8 +4,9 @@ use northwind;
 
 SELECT customers.customer_id AS identificador, customers.company_name AS nombre_empresa, COUNT(orders.order_id) AS numero_pedidos
 FROM customers
-CROSS JOIN orders
-WHERE customers.customer_id = orders.customer_id AND customers.country = "UK"
+INNER JOIN orders
+ON customers.customer_id = orders.customer_id
+WHERE customers.country = "UK"
 GROUP BY customers.company_name;
 
 # ejercicio 2: productos pedidos por empresa en UK por año:
