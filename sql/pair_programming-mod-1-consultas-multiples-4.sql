11d46cea1e5c5c0b51200fd0d0aeaf8a6b1601db
Ana Delia <anadeliats@gmail.com>
2022-07-28 13:09:23 +0200

    Comenzamos ejercicios consultas multiples 4

---
+++ sql/pair_programming-mod-1-consultas-multiples-4.sql
@@ -0,0 +1,53 @@
+USE northwind;
+
+-- EJERCICIO 1: Extraed información de los productos "Beverages"
+
+SELECT product_id, product_name, category_id
+FROM products
+WHERE category_id IN (
+SELECT category_id
+FROM categories
+WHERE category_name = "Beverages");
+
+-- EJERCICIO 2: Extraed la lista de países donde viven los clientes, 
+-- pero no hay ningún proveedor ubicado en ese país
+
+
+SELECT DISTINCT(country)
+FROM customers
+WHERE country NOT IN(
+SELECT DISTINCT(country)
+FROM suppliers);
+
+-- EJERCICIO 3: Extraer los clientes que compraron mas de 20 articulos "Grandma's Boysenberry Spread"
+
+
+SELECT order_id, customer_id
+FROM orders
+WHERE order_id IN
+(SELECT order_id
+FROM order_details
+WHERE quantity > 20 AND product_id IN
+(SELECT product_id
+FROM products
+WHERE product_name = "Grandma's Boysenberry Spread"))
+ORDER BY customer_id;
+
+-- EJERCICIO 4: Extraer los productos más caros
+
+
+
+SELECT product_name, unit_price
+FROM products
+WHERE unit_price IN (
+SELECT DISTINCT(unit_price)
+FROM order_details
+ORDER BY unit_price DESC
+LIMIT 10);
+
+-- BONUS -- Extraed cuál es el producto que más ha sido comprado y la cantidad que se compró
+
+SELECT product_name, unit_price
+FROM products
+ORDER BY unit_price DESC
+LIMIT 10;
