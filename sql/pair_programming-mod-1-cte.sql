21ffddd16bf0ec703570a3a5f4290134b909946a
antoanela31 <antoanelamateciuc@gmail.com>
2022-08-16 22:00:14 +0200

    ejercicio 3 y 4

--- sql/pair_programming-mod-1-cte.sql
+++ sql/pair_programming-mod-1-cte.sql
@@ -9,33 +9,45 @@ AS (
 SELECT *
 FROM companias_clientes;
 
+
 # EJERCICIO 2: Selecciona solo los de que vengan de "Germany"
 
 WITH companias_clientes
 AS (
-	SELECT company_name, customer_id
+	SELECT company_name, customer_id, country
     FROM customers
     WHERE country = 'Germany')
 SELECT *
 FROM companias_clientes;
 
+
 # EJERCICIO 3: Extraed el id de las facturas y su fecha de cada cliente.
 # En este caso queremos extraer todas las facturas que se han emitido a un cliente, su fecha, la compañia
 
-WITH compania (company_name)
-AS ( 
-SELECT company_name
-FROM customers AS c
-)
+WITH facturas_clientes (facturas_emitidas, fecha_factura, customer_id, compañía) 
+AS (SELECT orders.order_id, orders.order_date, customers.customer_id, customers.company_name
+	FROM orders
+    INNER JOIN customers
+    USING (customer_id))
+SELECT *
+FROM facturas_clientes;
 
 # EJERCICIO 4: Contad el número de facturas por cliente
+-- Mejoremos la query anterior. En este caso queremos saber el número de facturas emitidas por cada cliente.
 
+WITH facturas_clientes (facturas_emitidas, fecha_factura, customer_id, compañía) 
+AS (SELECT orders.order_id, orders.order_date, customers.customer_id, customers.company_name
+	FROM orders
+    INNER JOIN customers
+    USING (customer_id))
 SELECT customer_id, COUNT(order_id) AS cantidad_de_facturas
 FROM orders
 GROUP BY customer_id
 ORDER BY cantidad_de_facturas;
 
+
 # EJERCICIO 5: Cuál la cantidad media pedida de todos los productos ProductID.
+-- Necesitaréis extraer la suma de las cantidades por cada producto y calcular la media.
 
 WITH cantidad AS (
 SELECT  product_id,  SUM(quantity) AS cantidad_productos
@@ -43,10 +55,6 @@ FROM order_details
 GROUP BY product_id)
 SELECT AVG(cantidad_productos) AS cantidad_media
 FROM cantidad;
-
-
-
-
 
 
 
